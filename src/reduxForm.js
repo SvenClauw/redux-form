4822361b7bfa5632ee26d001924269044c02976a
SvenClauw <sven.clauw@gmail.com>
2016-10-31 14:34:41 +0100

    Call submitCompleted when onSubmit fails
    Fix forgotten this.

--- src/reduxForm.js
+++ src/reduxForm.js
@@ -295,7 +295,7 @@ const createReduxForm =
               return promise
             }
             this.submitPromise = promise
-            return promise.then(this.submitCompleted, submitCompleted)
+            return promise.then(this.submitCompleted, this.submitCompleted)
           }
 
           submit(submitOrEvent) {
